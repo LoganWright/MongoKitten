263e1e79ffa7d8f0007dd347b7af9ee394401738
Logan Wright <logan.william.wright@gmail.com>
2016-03-28 11:28:14 -0400

    c7 name

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "MongoKitten",
     dependencies: [
         .Package(url: "https://github.com/PlanTeam/BSON", majorVersion: 1, minor: 2),
-        .Package(url: "https://github.com/SwiftX/C7", majorVersion: 0, minor: 1),
+        .Package(url: "https://github.com/SwiftX/C7.git", majorVersion: 0, minor: 1),
         .Package(url: "https://github.com/ketzusaka/Hummingbird", majorVersion: 1, minor: 1),
     ]
 )
